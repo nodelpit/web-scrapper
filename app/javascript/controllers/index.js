8d6d0bb8ce237f4afb4e3675998e4e869b25abfb
Noah Delpit <noah02delpit@gmail.com>
2025-02-06 00:00:20 +0100

    Configurer Stimulus et résoudre les problèmes de bundling JavaScript avec Yarn Berry

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,8 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import { Application } from "@hotwired/stimulus"
+import { eagerLoadControllersFrom } from "@hotwired/stimulus"
+
+const application = Application.start()
 eagerLoadControllersFrom("controllers", application)
+
+export { application }
